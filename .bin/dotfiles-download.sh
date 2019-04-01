565f19a1c7549661cf9081d46eac2c3481cffe48
badarots <badarots@gmail.com>
2019-04-01 18:20:14 -0300

    Primeira versão

---
+++ .bin/dotfiles-download.sh
@@ -0,0 +1,15 @@
+git clone --bare https://github.com/badarot/dotfiles.git $HOME/.cfg
+function dotfiles {
+    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+dotfiles checkout
+if [ $? = 0 ]; then
+    echo "Checked out config.";
+else
+    echo "Backing up pre-existing dot files.";
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+dotfiles checkout
+dotfiles config status.showUntrackedFiles no
+echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.profile
---
+++ .profile
@@ -0,0 +1,6 @@
+export EDITOR=micro
+export VISUAL="$EDITOR"
+export PATH="${PATH}:/opt/anaconda/bin"
+alias lh='ls -lh'
+alias la='ls -a'
+alias dotfiles='/usr/bin/git --git-dir=/home/badaro/.dotfiles/ --work-tree=/home/badaro'
