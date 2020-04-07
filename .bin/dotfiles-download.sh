34d133e83fb1cfad794b7b4f2f21e73a91dea286
badarot <39627366+badarot@users.noreply.github.com>
2020-04-07 14:12:47 -0300

    Mais correções no script de download

--- .bin/dotfiles-download.sh
+++ .bin/dotfiles-download.sh
@@ -1,4 +1,4 @@
-git clone --bare https://github.com/badarot/dotfiles.git $HOME/.cfg
+git clone --bare https://github.com/badarot/dotfiles.git $HOME/.dotfiles
 function dotfiles {
     /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
@@ -8,7 +8,7 @@ if [ $? = 0 ]; then
     echo "Checked out config.";
 else
     echo "Backing up pre-existing dot files.";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles.bk/{}
 fi;
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
